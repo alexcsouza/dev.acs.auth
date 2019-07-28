262c0b61051678fc60228af65ce1c70c890c9895
Alex C. Souza <alexdcesouza@gmail.com>
2019-07-28 12:09:30 -0300

    Solved major problems of authentication

--- src/main/java/dev/acs/auth/module/login/LoginController.java
+++ src/main/java/dev/acs/auth/module/login/LoginController.java
@@ -1,32 +1,22 @@
-//package dev.acs.auth.module.login;
-//
-//import dev.acs.auth.module.user.service.IUserService;
-//import io.swagger.annotations.Api;
-//import io.swagger.annotations.ApiOperation;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.MediaType;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@Api(value = "Login")
-//@RequestMapping(path = {"/"})
-//public class LoginController {
-//
-//    @Autowired
-//    private IUserService userService;
-//
-//    @ApiOperation(value = "Get user information")
-//    @PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-//    public ResponseEntity<String> get(@RequestBody LoginDTO loginDTO) {
-//        String token = userService.authenticate(loginDTO);
-//        return ResponseEntity.ok(token);
-//    }
-//}
-//
+package dev.acs.auth.module.login;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Api(value = "Login")
+@RequestMapping(path = {"/"})
+public class LoginController {
+
+    @ApiOperation(value = "Get user information")
+    @PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<String> get(@RequestBody LoginDTO loginDTO) {
+    }
+}
+
