f29bf7d86a9e025a9af798ec8f2ca94dfc1cb4a6
Alex C. Souza <alexdcesouza@gmail.com>
2020-04-19 22:04:43 -0300

    Making small changes and work

--- src/main/java/dev/acs/auth/module/login/LoginDTO.java
+++ src/main/java/dev/acs/auth/module/login/LoginDTO.java
@@ -1,7 +1,9 @@
 package dev.acs.auth.module.login;
 
-import lombok.*;
-import lombok.experimental.SuperBuilder;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
--- src/main/java/dev/acs/auth/module/usergroup/UserGroup.java
+++ src/main/java/dev/acs/auth/module/usergroup/UserGroup.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
 
+import org.hibernate.envers.Audited;
+
 import dev.acs.auth.core.persistence.PersistentEntity;
 import dev.acs.auth.module.user.persistence.User;
 import lombok.Data;
@@ -15,12 +17,14 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 @Entity
 @EqualsAndHashCode(callSuper = false)
+@Audited
 public class UserGroup extends PersistentEntity {
 
 	private static final long serialVersionUID = -5033866583673683706L;
 	private String name;
 	private String description;
 	
+	@SuppressWarnings("deprecation")
 	@ManyToMany(mappedBy = "userGroups")
 	@org.hibernate.annotations.ForeignKey(name = "none")
 	private List<User> userList;
